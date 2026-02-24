a0e91d4804f8cbd609ca5d656c1fb837177cb680
ARYELY HEVYLYN DOS SANTOS SILVA <25203777@dsn127m1242267.cfp127aluno.local>
2026-02-24 11:29:39 -0300

    finalizando atividade aula 05

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ function adicionarProduto(){
 
     lista.appendChild(span)
 }
+
+function adicionarCodigo(){
+    const lista = document.getElementById("lista")
+    const codigo = document.getElementById("codigo")
+
+    const span = document.createElement("span")
+    span.textContent = codigo.value
+    span.className = "flex flex-col bg-blue-100 px-8 py-2"
+
+    lista.appendChild(span)
+}
+
+function adicionarQuantidade(){
+    const lista = document.getElementById("lista")
+    const quantia = document.getElementById("quantia")
+
+    const span = document.createElement("span")
+    span.textContent = quantia.value
+    span.className = "flex flex-col bg-blue-100 px-8 py-2"
+
+    lista.appendChild(span)
+}
